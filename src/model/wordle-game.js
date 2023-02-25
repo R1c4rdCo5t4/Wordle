1e2f934859113fe8650dd04b3994989a60d4a483
DQTree <dc.queijo01@gmail.com>
2023-02-25 12:58:00 +0000

    Frontend WIP

--- src/model/wordle-game.js
+++ src/model/wordle-game.js
@@ -7,19 +7,34 @@ function wordHint(guessWord, correctWord) {
 	if (guessWord.length != correctWord.length) {
 		throw new Error("Words must be the same length.")
 	}
-	let output = ""
+	let pairs = {}
 	for (let i = 0; i < guessWord.length; i++) {
-		if (guessWord[i] == correctWord[i]) output += colors.green + guessWord[i]
-		else if (correctWord.includes(guessWord[i])) output += colors.yellow + guessWord[i]
-		else output += colors.red + guessWord[i] 
+		const guessWLower = guessWord[i].toLowerCase()
+		const guessWUpper = guessWLower.toUpperCase() 
+		let color = null
+		switch(true) {
+			case guessWLower == correctWord[i]: 
+				color = colors.green; 
+				break;
+			case correctWord.includes(guessWLower): 
+				color = colors.yellow; 
+				break;
+			default: 
+				color = colors.red;
+		}
+		pairs[guessWUpper] = color
+		//if (guessWord[i] == correctWord[i]) output += colors.green + guessWord[i]
+		//else if (correctWord.includes(guessWord[i])) output += colors.yellow + guessWord[i]
+		//else output += colors.red + guessWord[i] 
 	}
-	output += colors.lightgrey
-	console.log(output)
+	console.log(pairs)
+	return pairs
 }
 
 export async function play(guess, game) {
 
-	wordHint(guess, game.word)
+	if(game.isOver) return game
+	const hints = wordHint(guess, game.word)
 	if (guess == game.word) {
 		console.log("You guessed the word!")
 		game.isOver = true
@@ -28,7 +43,7 @@ export async function play(guess, game) {
 	
 	console.log("Incorrect guess.")
 	game.guesses--
-	game.guessedWords.push(guess)
+	game.guessedWords.push(hints)
 	
 	if (game.guesses == 0) {
 		console.log("You are out of guesses.")
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,8 +1,16 @@
-
+/*
 export const colors = {
     red: '\x1b[31m',
     green: '\x1b[32m',
     yellow: '\x1b[93m',
     lightgrey: '\x1b[37m'
+}
+*/
+
+export const colors = {
+    red: 'red',
+    green: 'green',
+    yellow: 'yellow',
+    lightgrey: 'lightgrey'
 }
 
